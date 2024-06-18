16f0daad7ffe87d971c7eb699f7dab6dd3f57487
mbiswas <mbiswasnyc@gmail.com>
2024-06-18 00:23:26 -0400

    2nd Push

---
+++ src/test/java/CalculatorGas.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CalculatorGas {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void gasCalculator(){
+        startNavigation("https://www.calculator.net/");
+
+        findAndTypeByCss("#calcSearchTerm", "gas");
+        findAndClickByLinkedText("Gas Mileage Calculator");
+
+        findAndClickByCss("[value='Clear']");
+
+        findAndTypeByCss("#uscodreading", "68000");
+        findAndTypeByCss("#uspodreading", "67600");
+        findAndTypeById("usgasputin", "12");
+        findAndTypeById("usgasprice", "4.59");
+
+        findAndClickByCss("[name='x']");
+
+        Assert.assertEquals(findAndGetTextByCss("[class='h2result']"), "Result");
+
+    }
+
+     @AfterMethod
+       public void tearDown(){
+           driver.close();
+       }
+
+    private void startNavigation(String url){
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    private void implicitlyWait(int sec){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(sec));
+    }
+
+    private void findAndClickByCss(String css){
+        driver.findElement(By.cssSelector(css)).click();
+    }
+
+    private void findAndTypeByCss(String css, String text){
+        driver.findElement(By.cssSelector(css)).sendKeys(text);
+    }
+
+    private void findAndTypeById(String id, String text){
+        driver.findElement(By.id(id)).sendKeys(text);
+    }
+
+    private String findAndGetTextByCss(String css){
+        String text = driver.findElement(By.cssSelector(css)).getText();
+        return text;
+    }
+
+    private void findAndClickByLinkedText(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
